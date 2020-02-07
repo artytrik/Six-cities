3752c8b0aba145082b1ab39660e99d0e47a6090f
artytrik <tema-luch@mail.ru>
2020-02-07 10:37:39 +0300

    Подключает prop-types

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
 const RENTAL_OFFERS = 10;
+const RENTAL_NAMES = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`];
 
 ReactDOM.render(
     <App
       rentalOffers={RENTAL_OFFERS}
+      rentalNames={RENTAL_NAMES}
     />,
     document.querySelector(`#root`)
 );
