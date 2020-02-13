6176a78809612fd1a7984831b83be4543460a3e2
artytrik <tema-luch@mail.ru>
2020-02-13 22:23:08 +0300

    Наполняет моки данными

---
+++ src/mocks/offers.js
@@ -0,0 +1,30 @@
+export const offers = [
+  {
+    name: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    price: 120,
+    picture: `img/apartment-01.jpg`,
+    premium: true
+  },
+  {
+    name: `Wood and stone place`,
+    type: `Private room`,
+    price: 80,
+    picture: `img/room.jpg`,
+    premium: false
+  },
+  {
+    name: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    price: 132,
+    picture: `img/apartment-02.jpg`,
+    premium: false
+  },
+  {
+    name: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    price: 180,
+    picture: `img/apartment-03.jpg`,
+    premium: true
+  },
+];
