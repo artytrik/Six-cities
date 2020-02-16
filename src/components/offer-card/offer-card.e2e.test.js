c159f1d2f74bd262caff042ad81544c45886ffae
artytrik <tema-luch@mail.ru>
2020-02-16 14:18:46 +0300

    Делает e2e тесты для обработчика при клике на заголовок предложения

--- src/components/offer-card/offer-card.e2e.test.js
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -32,3 +32,19 @@ it(`Correct information should be in OfferCard after hover`, () => {
   expect(onMouseHover.mock.calls[0][0]).toMatchObject(offer);
 });
 
+it(`Correct information should be in OfferCard after click on header`, () => {
+  const onMouseHover = jest.fn();
+  const onHeaderClick = jest.fn();
+  const offerCard = shallow(<OfferCard
+    offer={offer}
+    onMouseHover={onMouseHover}
+    onHeaderClick={onHeaderClick}
+  />);
+
+  const offerHeader = offerCard.find(`.place-card__name a`);
+  offerHeader.simulate(`click`);
+
+  expect(onHeaderClick).toHaveBeenCalledTimes(1);
+  expect(onHeaderClick.mock.calls[0][0]).toMatchObject(offer);
+});
+
