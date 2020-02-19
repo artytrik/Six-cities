ce978a0c8ae5cf70fe93e7d53c2c1191649d68a8
artytrik <tema-luch@mail.ru>
2020-02-19 17:22:54 +0300

    Делает компонент карты и подключает к главной странице

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import OffersList from '../offers-list/offers-list.jsx';
+import Map from '../map/map.jsx';
 
 const Main = (props) => {
   const {offersNumber, offers, onHeaderClick} = props;
@@ -94,7 +95,9 @@ const Main = (props) => {
               />
             </section>
             <div className="cities__right-section">
-              <section className="cities__map map"></section>
+              <Map
+                offers={offers}
+              />
             </div>
           </div>
         </div>
