63c36e7b4d54d38f14bd9cf6d3d0f1244838e428
artytrik <tema-luch@mail.ru>
2020-02-24 13:20:36 +0300

    Добавляет хранилище и подключает компонент App к нему

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
+import {createStore} from 'redux';
+import {reducer} from './reducer.js';
+import {Provider} from 'react-redux';
 import {offers, OFFERS_NUMBER} from './mocks/offers.js';
 
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
+
 ReactDOM.render(
-    <App
-      offersNumber={OFFERS_NUMBER}
-      offers={offers}
-    />,
+    <Provider store={store}>
+      <App
+        offersNumber={OFFERS_NUMBER}
+        offers={offers}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
