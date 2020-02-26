54c910bb0541d6adb4ab29c6a37caeb3390262b4
artytrik <tema-luch@mail.ru>
2020-02-26 23:07:03 +0300

    Дополняет компонент сортировки обработчиками

--- src/components/sorting-options/sorting-options.jsx
+++ src/components/sorting-options/sorting-options.jsx
@@ -1,23 +1,74 @@
 import React from 'react';
 
-const SortingOptions = () => {
-  return (
-    <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex="0">
-        Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
-          <use xlinkHref="#icon-arrow-select"></use>
-        </svg>
-      </span>
-      <ul className="places__options places__options--custom">
-        <li className="places__option places__option--active" tabIndex="0">Popular</li>
-        <li className="places__option" tabIndex="0">Price: low to high</li>
-        <li className="places__option" tabIndex="0">Price: high to low</li>
-        <li className="places__option" tabIndex="0">Top rated first</li>
-      </ul>
-    </form>
-  );
+const SortType = {
+  POPULAR: `Popular`,
+  PRICE_LOW_TO_HIGH: `Price: low to high`,
+  PRICE_HIGH_TO_LOW: `Price: high to low`,
+  TOP_RATED: `Top rated first`
 };
+
+class SortingOptions extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentSortType: SortType.POPULAR,
+      isOpened: false
+    };
+
+    this._handleSortClick = this._handleSortClick.bind(this);
+    this._handleSortTypeClick = this._handleSortTypeClick.bind(this);
+  }
+
+  _handleSortClick() {
+    this.setState({
+      isOpened: !this.state.isOpened
+    });
+  }
+
+  _handleSortTypeClick(sortType) {
+    this.setState({
+      currentSortType: sortType
+    });
+  }
+
+  render() {
+    const {currentSortType, isOpened} = this.state;
+    return (
+      <form className="places__sorting" action="#" method="get">
+        <span className="places__sorting-caption">Sort by</span>
+        <span
+          className="places__sorting-type"
+          tabIndex="0"
+          onClick={this._handleSortClick}
+        >
+          {currentSortType}
+          <svg className="places__sorting-arrow" width="7" height="4">
+            <use xlinkHref="#icon-arrow-select"></use>
+          </svg>
+        </span>
+        <ul
+          className={`places__options places__options--custom
+            ${isOpened && `places__options--opened`}`}
+          onClick={this._handleSortClick}
+        >
+          {Object.values(SortType).map((item, i) => {
+            return (
+              <li
+                key={`item-${i}`}
+                className={`places__option
+                  ${(item === currentSortType) && `places__option--active`}`}
+                tabIndex="0"
+                onClick={() => this._handleSortTypeClick(item)}
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </form>
+    );
+  }
+}
 
 export default SortingOptions;
