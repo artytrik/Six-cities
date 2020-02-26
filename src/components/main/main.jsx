ff29a430cff9a1f25db128e77ac9621612b9c244
artytrik <tema-luch@mail.ru>
2020-02-26 13:09:27 +0300

    Делает компонент вариантов сортировки

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import OffersList from '../offers-list/offers-list.jsx';
 import Map from '../map/map.jsx';
 import CitiesList from '../cities-list/cities-list.jsx';
+import SortingOptions from '../sorting-options/sorting-options.jsx';
 
 const Main = (props) => {
   const {offers, onHeaderClick, city, cities, onCityClick} = props;
@@ -50,21 +51,7 @@ const Main = (props) => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{offers.length} places to stay in {city}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
+              <SortingOptions />
               <OffersList
                 className="cities__places-list tabs__content"
                 offers={offers}
