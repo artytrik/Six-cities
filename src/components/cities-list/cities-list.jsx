8b52bdf5ed84b5f96fc314782e523d0903ab5197
artytrik <tema-luch@mail.ru>
2020-03-01 09:42:40 +0300

    Делает компонент списка городов функциональным

--- src/components/cities-list/cities-list.jsx
+++ src/components/cities-list/cities-list.jsx
@@ -1,35 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class CitiesList extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const CitiesList = (props) => {
+  const {onCityClick, city: activeCity, cities} = props;
 
-  render() {
-    const {onCityClick, city: activeCity, cities} = this.props;
-
-    return <ul className="locations__list tabs__list">
-      {cities.map((city) => {
-        const className = `locations__item-link tabs__item ${(city === activeCity) && `tabs__item--active`}`;
-        return (
-          <li
-            className="locations__item"
-            key={city}
+  return <ul className="locations__list tabs__list">
+    {cities.map((city) => {
+      const className = `locations__item-link tabs__item ${(city === activeCity) ? `tabs__item--active` : ``}`;
+      return (
+        <li
+          className="locations__item"
+          key={city}
+        >
+          <a
+            className={className}
+            href="#"
+            onClick={(evt) => onCityClick(evt, city)}
           >
-            <a
-              className={className}
-              href="#"
-              onClick={(evt) => onCityClick(evt, city)}
-            >
-              <span>{city}</span>
-            </a>
-          </li>
-        );
-      })}
-    </ul>;
-  }
-}
+            <span>{city}</span>
+          </a>
+        </li>
+      );
+    })}
+  </ul>;
+};
 
 CitiesList.propTypes = {
   onCityClick: PropTypes.func.isRequired,
